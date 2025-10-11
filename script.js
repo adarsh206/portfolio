6c9357a67aa8dd7bf02d6ce2f7541378a4d9ef89
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-10-11 18:22:07 +0530

    hero section completed

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const sideMenu = document.querySelector("#sideMenu");
+
+function openMenu(){
+    sideMenu.style.transform = 'translateX(-16rem)';
+}
+
+function closeMenu(){
+    sideMenu.style.transform = 'translateX(16rem)';
+}
